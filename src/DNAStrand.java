bc6b83875816be34e783f22d83fe144ab94bba36
Srdjan Miletic <SrdjanMiletic2@gmail.com>
2016-10-23 00:58:12 +0100

    Added complement and Complement Paliendrome functions

--- src/DNAStrand.java
+++ src/DNAStrand.java
@@ -1,6 +1,7 @@
 public class DNAStrand {
 	
 	private String base;
+	StringBuilder s = new StringBuilder();
 	
 	public DNAStrand(String DNA){
 		this.base = DNA;
@@ -19,5 +20,34 @@ public class DNAStrand {
 		}
 		
 		return true;
+	}
+	
+	public String complementWC(){
+		s.setLength(0);
+		for (int i = 0; i < base.length(); i++) {
+			char z = base.charAt(i);
+			if(z == 'A'){
+				s.append('T');
+			}
+			if(z == 'T'){
+				s.append('A');
+			}
+			if(z == 'G'){
+				s.append('C');
+			}
+			if(z == 'C'){
+				s.append('G');
+			}
+		}
+		
+		return(s.toString());
+	}
+	
+	public String palindromeWC(){
+		s.setLength(0);
+		String palindrome = this.complementWC();
+		s.append(palindrome);
+		s.reverse();
+		return(s.toString());
 	}
 }
